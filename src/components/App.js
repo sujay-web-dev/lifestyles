8577ee6dcae894a11be06cf33f1408601a452e27
Sujay V <sujaywebdev@gmail.com>
2021-09-12 21:11:29 +0530

    The AppBar Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import Header from "./ui/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Sujay</h1>
     </div>
   );
---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from "@material-ui/core";
+import React from "react";
+
+function Header() {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>LifeStyles</Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
