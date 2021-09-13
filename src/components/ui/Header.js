04f78efce2ce9dc7df19a3816d80ed0c6f0295a7
Sujay V <sujaywebdev@gmail.com>
2021-09-13 09:35:52 +0530

    7. AppBar Transitions

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,27 @@
 import { AppBar, Toolbar } from "@material-ui/core";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import React from "react";
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
 
 function Header() {
   return (
-    <AppBar position="fixed">
-      <Toolbar>LifeStyles</Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>LifeStyles</Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 }
 
