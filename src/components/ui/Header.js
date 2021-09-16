71bfea2d2d576bb157fce2ebe6b81f46daadd17d
Sujay V <sujaywebdev@gmail.com>
2021-09-16 12:40:57 +0530

    19. Styling The Active Tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar } from "@material-ui/core";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
 
 import logo from "../../assets/logo.svg";
 
@@ -26,10 +29,31 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: "4em",
   },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+    // height: 60,
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "20px",
+    height: "45px",
+  },
 }));
 
 function Header() {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
 
   return (
     <>
@@ -37,6 +61,25 @@ function Header() {
         <AppBar position="fixed">
           <Toolbar disableGutters>
             <img src={logo} alt="Company Logo" className={classes.logo} />
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="primary"
+            >
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -18,8 +18,17 @@ export default createTheme({
     },
   },
   typography: {
-    h3: {
-      fontWeight: 300,
+    tab: {
+      fontFamily: "Raleway",
+      textTransform: "none",
+      fontWeight: 700,
+      fontSize: "1rem",
+    },
+    estimate: {
+      fontFamily: "Pacifico",
+      fontSize: "1rem",
+      textTransform: "none",
+      color: "white",
     },
   },
 });
